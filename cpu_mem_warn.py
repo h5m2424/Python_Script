86f134a315736fcd6b55a8cc340170564d32b569
h5m2424 <h5m2424@qq.com>
2022-07-01 16:49:23 +0800

    change mail lib to yagmail

--- cpu_mem_warn.py
+++ cpu_mem_warn.py
@@ -1,55 +1,52 @@
 """
 # mail example
-2020/06/22-20:48:01
-[CPU_Used]: 10.6 %
+
+[Server]: Server_1
+[Time]: 2020/06/22-20:48:01
+[CPU_Used]: 16 %
 [MEM_Available]: 3562 MB (44 %)
 """
 
-import smtplib
 import time
 import psutil
-import logging;logging.basicConfig(level=logging.INFO)
-from email.mime.text import MIMEText
- 
-def send(time_formated, detials):
-    msg = MIMEText(time_formated + "\n[CPU_Used]: " + detials[0] + "\n[MEM_Available]: " + detials[1])
-    logging.debug(msg.as_string())
-    msg["Subject"] = subject
-    msg["From"] = e_from
-    msg["To"] = e_to
+import yagmail
+import socket
 
-    server = smtplib.SMTP(host, port)
-    try:
-        server.login(e_from, password)
-        server.sendmail(e_from, e_to, msg.as_string())
-        logging.info("send success" + "\n")
-    except Exception as e:
-        logging.error("send fail", e + "\n")
-    finally:
-        server.quit()
- 
-e_from = "*****@aliyun.com"
-password = "******"
-e_to = "*****@qq.com"
-host = "smtp.qiye.aliyun.com"
-port = 25
-subject = "cpu & 内存使用率警告"
-cpu_warning_point = 65   # 单位：%（大于此值发送警告）
-mem_warning_point = 20  # 单位：%（小于此值发送警告）
-check_time = 5    # 单位：秒（检测间隔）
+e_from = "xxx@qq.com"
+e_password = "xxxxxx"
+e_host = "smtp.qiye.aliyun.com"
+e_to = ['xxx@qq.com','xxx@hotmail.com']
+e_cc = "xxx@qq.com"
+#e_bcc = ""
+e_subject = "cpu & 内存使用率警告"
+e_attachments = []
+cpu_warning_point = 80   # 单位：%（大于此值发送警告）
+mem_warning_point = 10  # 单位：%（小于此值发送警告）
+check_time = 60    # 单位：秒（检测间隔）
+hostname = socket.gethostname()
+
+def send(time_formated, detials):
+    
+    #创建yagmail.SMTP实例
+    yag = yagmail.SMTP(user = e_from, password = e_password, host = e_host) #SMTP服务器域名
+
+    yag.send(to = e_to,
+         cc = e_cc, #抄送
+         #bcc = e_bcc, #密送
+         subject = hostname + " | " + e_subject,
+         contents = "[Server]: " + hostname + "\n[Time]: " + time_formated + "\n[CPU_Used]: " + detials[0] + "\n[MEM_Available]: " + detials[1],
+         attachments = e_attachments)
  
 while True:
     time.sleep(check_time)
     time_formated = time.strftime("%Y/%m/%d-%H:%M:%S")
     cpu_percent = int(psutil.cpu_percent())
-    avalb_mem = int(psutil.virtual_memory().available) / 1024 / 1024    # 单位换算成 MB
+    avalb_mem = int(int(psutil.virtual_memory().available) / 1024 / 1024)    # 单位换算成 MB
     mem_percent = int(psutil.virtual_memory().percent)
     detials = (str(cpu_percent) + " %", str(avalb_mem) + " MB" + " (" + str(100 - mem_percent) + " %" + ")")
-    logging.info(time_formated)
-    logging.info("[CPU_Used]: " + detials[0])
-    logging.info("[MEM_Available]: " + detials[1])
  
     if cpu_percent >= cpu_warning_point or (100 - mem_percent) <= mem_warning_point:
         send(time_formated, detials)
+        print("Warning mail sent: " + "\n[Time]: " + time_formated + "\n[CPU_Used]: " + detials[0] + "\n[MEM_Available]: " + detials[1] + "\n")
     else:
-        print("passed" + "\n")
+        print("passed: " + "\n[Time]: " + time_formated + "\n[CPU_Used]: " + detials[0] + "\n[MEM_Available]: " + detials[1] + "\n")
